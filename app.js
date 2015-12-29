3631a4c15931f4943007d7e94f40b9de77d179ee
steven <slstevenlam@gmail.com>
2015-12-29 12:31:35 -0800

    reformated output

--- app.js
+++ app.js
@@ -6,10 +6,7 @@ var fs = require("fs");
 var cheerio = require('cheerio');
 var request = require('request');
 var xls = require('excel');
-
-
-
-
+var util = require('util')
 
 function convertToJSON(array) {
   var first = array[0].join()
@@ -33,31 +30,34 @@ function convertToJSON(array) {
   return jsonData;
 };
 
+var arr = [];
+var json;
 
 function get_item(url){
-  var arr = [];
+
   request(url, function(error, response, html){
   //check for errors
     if(!error){
       //utilize the cheerio library on returned html
       var $ = cheerio.load(html);
       //define variable we are going to capture
-      var product;
-      var json = {product_name: ""}
+
       //use the unique header class as a starting point
       $('.it-ttl').filter(function(){
         //store data we filter into a variable
         var data = $(this);
-
+        // json = {product_name: ""};
         //examine the DOM
         title = data.text().slice(16);
         //store title to json object
         json = {
           product_name: title
         }
+        arr.push(json);
+        // console.log("inside method", json);
       })
-      console.log(arr);
-      fs.appendFile("output.json", JSON.stringify(json, null, 4), function(err){
+      console.log(arr)
+      fs.writeFileSync('output.json', JSON.stringify(arr, null, 4), 'utf-8', function(err){
         console.log("fille succesfully written")
       })
     }
@@ -80,7 +80,7 @@ app.get('/scrape', function(req, res){
         url = original_json[item].ebay_url;
         console.log("URLL", url);
 
-        get_item(url);
+        var x = get_item(url);
         // To write to the system we will use the built in 'fs' library.
         // In this example we will pass 3 parameters to the writeFile function
         // Parameter 1 :  output.json - this is what the created filename will be called
@@ -91,9 +91,9 @@ app.get('/scrape', function(req, res){
       // })
 
       }
-      console.log(arr);
     }
   })
+  res.send('Check your console!')
 })
 
 
@@ -105,7 +105,6 @@ app.use(bodyParser.json());
 
 // set up a static file server that points to the "client" directory
 app.use(express.static(path.join(__dirname, './client')));
-
 
 app.listen(8000, function() {
   console.log('cool stuff on: 8000');
