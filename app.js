dc0a1b8cbd1416e1a6b026a89398fb9152275fd4
steven <slstevenlam@gmail.com>
2016-02-01 22:47:19 -0800

    setup schema and search

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var json2csv    = require('json2csv');
 var Grid        = require('gridfs-stream');
 var mongo       = require('mongodb');
 
+var dateFormat  = require('dateFormat');
 // set up a static file server that points to the "client" directory
 app.use(express.static(path.join(__dirname, './client')));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -31,11 +32,14 @@ var storage = multer.diskStorage({
     callback(null, './uploads/')
   },
   filename: function(req, file, callback){
-    callback(null, "product_list")
+    var d = new Date()
+    var formated_date = dateFormat(d, "mmddyyyy");
+    callback(null, file.fieldname + '-' + formated_date)
   }
 })
 
-var upload = multer({storage: storage});
+ var upload = multer({storage: storage});
+
 
 var mongoose = require('mongoose');
 // database name
@@ -46,68 +50,89 @@ var gfs = Grid(conn.db);
 
 
 var Schema = mongoose.Schema;
-
 var FileSchema = new mongoose.Schema({
   scan_date: {type: Date, default: new Date},
-  scan_name: String,
+  file_name: String,
+  user: String,
   original: [],
   output: []
 })
+
 mongoose.model('File', FileSchema);
 var File = mongoose.model('File')
 
+var ResSchema = new mongoose.Schema({
+  file_name: String,
+  scan_date: {type: Date, default: new Date},
+  user: String,
+  result: []
+})
 
+mongoose.model('Res', ResSchema);
+var Res = mongoose.model('Res');
 
 //User uploads formated CSV/xlsx file. Make sure column headers are formated with no spaces.
 // 'product_name',  'Item_ID',   'Item_Condition', 'ebay_status',  'Vertical',  'Seller_Name', 'Account_Manager', 'MSRP', 'ebay_msrp',  'list_price', 'ebay_list_price', 'recal_perc_off', '%_off', 'Sum_of_GMV',  'Sum_of_Qty',  'Sum_of_Views/SI', 'Sum_of_Seller_rating',  'Sum_of_Buyer_Count',  'Sum_of_Defect_Rate'
-app.post('/upload', upload.single('file'), function(req, res){
+app.post('/upload', upload.single('myFile'), function(req, res){
+  console.log("upload", req.file)
   var path = req.file.path;
   var source = fs.createReadStream(path);
-  var writestream = gfs.createWriteStream({
-    filename: req.file.filename
-  });
-  source.pipe(writestream);
 
-  gfs.files.findOne({filename: "product_list"}, function(err, file){
 
-  var readstream = gfs.createReadStream({
-    filename: file.filename
-  })
+  //check if files already exist
+  var options = {
+    filename: req.file.originalname
+  }
+  gfs.exist(options, function (err, found) {
+  if (err) return handleError(err);
+  if (found){
+    console.log("File exists");
+    res.json({success: true, file_name: req.file.originalname});
+  } else {
+      console.log("file does not exist")
+      var writestream = gfs.createWriteStream({
+        filename: req.file.originalname
+      });
 
-    //call convertToJSON method to change CSV to right format
-    // convert excel to json
-    xls(readstream, function(err, data) {
-      if(err) {
-        console.log("error converting")
-      }
-      else {
-        //call convertToJSON method to change CSV to right format
-        original_json = convertToJSON(data);
-
-        var file = new File ({
-          scan_name: "0000",
-          original: original_json
+      source.pipe(writestream);
+      //find uploaded file
+      writestream.on('close', function (file) {
+        // do something with `file`
+        console.log(file.filename);
+        gfs.files.findOne({filename: req.file.originalname}, function(err, file){
+        console.log("FILE", file)
+        var readstream = gfs.createReadStream({
+        filename: file.filename
         })
-        file.save(function(err, result){
-          if(err){
-            console.log("document did no save");
+
+        //pass readstream to xls to read file
+        //call convertTOmethod to change data to JSON
+        xls(readstream, function(err, data) {
+          if(err) {
+            console.log("error converting")
           } else {
-            console.log("document saved", result);
-            res.json({success: true});
-          }
+            original_json = convertToJSON(data);
+            //save JSON to db
+            var file = new File ({
+              file_name: req.file.originalname,
+              user: req.body.email,
+              original: original_json
+            })
+            file.save(function(err, result){
+              if(err){
+                console.log("document did no save");
+              } else {
+                console.log("document saved", result);
+                res.json({success: true, file_name: req.file.originalname});
+              }
+            })
+            }
+          })
         })
-
-        // fs.writeFile("original.json", JSON.stringify(original_json, null, 4),
-
-        // function(err){
-        //   console.log("fille succesfully written");
-        //   res.json({success: true});
-        // })
-      }
-    })
+      });
+    }
   })
 })
-
 
 app.get('/export', function(req, res){
   var result = './client/static/json/result.json';
@@ -152,181 +177,193 @@ app.get('/export', function(req, res){
 
 //user selects "get results on frontend"
 //compare original.json with output.json
-app.get('/get_results', function(req, res){
-  get_item(function(data){
-    File.findOne({scan_name: "0000"}, function(err, result){
+app.get('/get_results/:id', function(req, res){
+  console.log("filename", req.params.id)
 
-    var output = result.output[0];
-    var original = result.original[0];
+  get_item(req.params.id, function(data){
 
+    File.findOne({file_name: req.params.id}, function(err, result){
+    var output = result.output;
+    var original = result.original;
+    console.log("LENGTHHHH_______", output.length, original.length)
     var sorted_output = _.sortBy(output, 'ebay_Item_ID');
+    sorted_output.shift();
     var sorted_original = _.sortBy(original, 'Item_ID');
 
     for(var x in sorted_original){
       if(sorted_original[x].Item_ID != undefined){
-
         sorted_output[x] = _.extend(sorted_output[x], sorted_original[x]);
-
-        var check_id = _.isMatch(sorted_original[x], sorted_output[x].Item_ID)
-        if(check_id){
-          for(var y in sorted_original[x]){
-            switch (true) {
-              case (y === 'product_name'):
-                if(sorted_original[x].product_name == sorted_output[x].ebay_product_name){
-                  sorted_output[x].check_product_name = "pass";
-                }
-                else{
-                  sorted_output[x].check_product_name = "fail";
-                }
-              case (y === 'list_price'):
-                if(sorted_original[x].list_price == sorted_output[x].ebay_list_price){
-                  sorted_output[x].check_list_price = "pass";
-                }
-                else{
-                  sorted_output[x].check_list_price = "fail";
-                }
-              break;
-            }
-          }
-        }
       }
     }
-    console.log(sorted_output);
-    fs.writeFile("./client/static/json/result.json", 'var result = ' + JSON.stringify(sorted_output, null, 4), function(err){
-      console.log("fille succesfully written")
-      })
+
+    Res.findOne({file_name: result.originalname}, function(err, file){
+      console.log("SDF", file)
+      if(file == null){
+        console.log("sorted_output", sorted_output);
+        var new_res = new Res({
+          user: result.user,
+          file_name: result.file_name,
+          result: sorted_output
+        })
+        new_res.save(function(err, result){
+          if(err){
+            console.log("result did no save");
+          } else {
+            res.send({result: sorted_output});
+          }
+        })
+      } else {
+        res.send({result: sorted_output});
+      }
     })
-    console.log("END")
-    res.status(204).end();
+  })
+
+    // fs.writeFile("./client/static/json/result.json", 'var result = ' + JSON.stringify(sorted_output, null, 4), function(err){
+    //   console.log("fille succesfully written")
+    //   })
+    // })
+
+  })
+})
+
+app.get('/search_results/:id', function(req, res){
+  console.log(req.params)
+  Res.findOne({file_name: req.params.id}, function(err, data){
+    if(err){
+      console.log("cant find")
+    } else {
+      console.log(data)
+      res.send({result: data});
+    }
   })
 })
 
 //method loops through array of objects in original.json to grab url
 //pass url to ebay API to gather info
-function get_item(callback2){
+function get_item(filename, callback2){
   // var original = 'original.json'
   // var original_json = jsonfile.readFileSync(original);
-  File.findOne({scan_name: "0000"}, function(err, result){
+  File.findOne({file_name: filename}, function(err, result){
     if(err){
       console.log("can not find document");
     } else {
-  var original_json = result.original;
-  var array_of_urls = [];
+      console.log("RESULT", result)
+        var original_json = result.original;
+        var array_of_urls = [];
 
-  for(var item in original_json){
-    url = original_json[item].Item_ID;
-    array_of_urls.push(url);
-  }
-
-  //asyncjs library to make synchronous call to ebay api
-  var arr = [];
-  async.eachSeries(array_of_urls,
-    function(url, callback){
-     var json = {
-        ebay_Item_ID: "",
-        ebay_product_name: "",
-        ebay_list_price: "",
-        ebay_msrp: "",
-        ebay_status: "",
-        galleryURL: "",
-        recal_perc_off: ""
-      }
-      var params = {
-        keywords: [url],
-        // add additional fields
-        outputSelector: ['AspectHistogram'],
-
-        paginationInput: {
-          entriesPerPage: 1
+        for(var item in original_json){
+          url = original_json[item].Item_ID;
+          array_of_urls.push(url);
         }
-      };
-      //ebay request
-      ebay.xmlRequest({
-        serviceName: 'Finding',
-        opType: 'findItemsByKeywords',
-        appId: 'RideSnap-b66a-448f-9063-46ba6dbe1a3e',
-        params: params,
-        parser: ebay.parseResponseJson    // (default)
-      },
-      // gets all the items together in a merged array
-      function itemsCallback(error, itemsResponse) {
-        console.log("FAILING", itemsResponse.searchResult)
-        //push empty object if failure
-        if(itemsResponse.ack == "Failure"){
-          json.ebay_Item_ID = url;
-          arr.push(json);
-          callback();
-        }
-        //if ebay returns count of 0, then manually grab product listing by crawling website with cheerio
-        else if(itemsResponse.searchResult.$.count == 0){
-          request("http://ebay.com/itm/"+url, function(error, response, html){
-          //check for errors
-            if(!error){
-              //utilize the cheerio library on returned html
-              var $ = cheerio.load(html);
-              var id = url.slice(20);
-              //get product name using unique class as starting point
-              $('.it-ttl').filter(function(){
-              var data = $(this);
-              title = data.text().slice(16);
-              json.ebay_product_name = title;
-              })
-              //grab status and item id with unique class
-              $('.msgTextAlign').filter(function(){
-                var data = $(this);
-                status = data.text()
-                json.ebay_status = status;
-                json.ebay_Item_ID = url;
-              })
+
+        //asyncjs library to make synchronous call to ebay api
+        var arr = [];
+        async.eachSeries(array_of_urls,
+          function(url, callback){
+           var json = {
+              ebay_Item_ID: "",
+              ebay_product_name: "",
+              ebay_list_price: "",
+              ebay_msrp: "",
+              ebay_status: "",
+              galleryURL: "",
+              recal_perc_off: ""
             }
-          arr.push(json);
-          callback();
-          })
-        }
-        else{ //if we are able to find product with ebay call and count is 0 then store info
-          // console.log("a;lsdkjf;alksdjf", itemsResponse.searchResult.item.discountPriceInfo)
-        json = {
-          ebay_Item_ID: itemsResponse.searchResult.item.itemId,
-          ebay_product_name: itemsResponse.searchResult.item.title,
-          ebay_list_price: itemsResponse.searchResult.item.sellingStatus.currentPrice.amount,
-          ebay_status: itemsResponse.searchResult.item.sellingStatus.sellingState,
-          galleryURL: itemsResponse.searchResult.item.galleryURL
-        }
-        //this is to check for msrp.
-        if(_.has(itemsResponse.searchResult.item, "discountPriceInfo")){
-          if(_.has(itemsResponse.searchResult.item.discountPriceInfo, "originalRetailPrice")){
-          json.ebay_msrp = itemsResponse.searchResult.item.discountPriceInfo.originalRetailPrice.amount
-        }
-        }
-        arr.push(json)
-        callback();
-        }
-      });
-    },
-    function (error){
-      File.findOne({scan_name: "0000"}, function(err, data){
-        console.log("DA", data.output)
-        data.output.push(arr);
+            var params = {
+              keywords: [url],
+              // add additional fields
+              outputSelector: ['AspectHistogram'],
 
-        data.save(function(err, res){
-        if(err){
-          console.log("output did not save");
-        } else {
-          console.log("output saved");
-        }
-       })
-      });
-      // JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make JSON easier to read
-      // Parameter 3 :  callback function - a callback function to let us know the status of our
-      fs.writeFile('output.json', JSON.stringify(arr, null, 4), 'utf-8',
-      function(err){
-        if(err){
-          console.log("fille error when writing")
-        }
-       callback2("complete")
-      })
-    })
-  console.log("orig leng", original_json.length)
+              paginationInput: {
+                entriesPerPage: 1
+              }
+            };
+            //ebay request
+            ebay.xmlRequest({
+              serviceName: 'Finding',
+              opType: 'findItemsByKeywords',
+              appId: 'RideSnap-b66a-448f-9063-46ba6dbe1a3e',
+              params: params,
+              parser: ebay.parseResponseJson    // (default)
+            },
+            // gets all the items together in a merged array
+            function itemsCallback(error, itemsResponse) {
+              console.log("FAILING", itemsResponse.searchResult)
+              //push empty object if failure
+              if(itemsResponse.ack == "Failure"){
+                json.ebay_Item_ID = url;
+                arr.push(json);
+                callback();
+              }
+              //if ebay returns count of 0, then manually grab product listing by crawling website with cheerio
+              else if(itemsResponse.searchResult.$.count == 0){
+                request("http://ebay.com/itm/"+url, function(error, response, html){
+                //check for errors
+                  if(!error){
+                    //utilize the cheerio library on returned html
+                    var $ = cheerio.load(html);
+                    var id = url.slice(20);
+                    //get product name using unique class as starting point
+                    $('.it-ttl').filter(function(){
+                    var data = $(this);
+                    title = data.text().slice(16);
+                    json.ebay_product_name = title;
+                    })
+                    //grab status and item id with unique class
+                    $('.msgTextAlign').filter(function(){
+                      var data = $(this);
+                      status = data.text()
+                      json.ebay_status = status;
+                      json.ebay_Item_ID = url;
+                    })
+                  }
+                arr.push(json);
+                callback();
+                })
+              }
+              else{ //if we are able to find product with ebay call and count is 0 then store info
+                // console.log("a;lsdkjf;alksdjf", itemsResponse.searchResult.item.discountPriceInfo)
+              json = {
+                ebay_Item_ID: itemsResponse.searchResult.item.itemId,
+                ebay_product_name: itemsResponse.searchResult.item.title,
+                ebay_list_price: itemsResponse.searchResult.item.sellingStatus.currentPrice.amount,
+                ebay_status: itemsResponse.searchResult.item.sellingStatus.sellingState,
+                galleryURL: itemsResponse.searchResult.item.galleryURL
+              }
+              //this is to check for msrp.
+              if(_.has(itemsResponse.searchResult.item, "discountPriceInfo")){
+                if(_.has(itemsResponse.searchResult.item.discountPriceInfo, "originalRetailPrice")){
+                json.ebay_msrp = itemsResponse.searchResult.item.discountPriceInfo.originalRetailPrice.amount
+                }
+              }
+              arr.push(json)
+              callback();
+              }
+            });
+          },
+          function (error){
+            File.findOne({file_name: filename}, function(err, data){
+              data.output = arr;
+              data.save(function(err, res){
+              if(err){
+                console.log("output did not save");
+              } else {
+                console.log("output saved");
+                callback2("complete")
+                }
+             })
+          });
+          // JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make JSON easier to read
+          // Parameter 3 :  callback function - a callback function to let us know the status of our
+          // fs.writeFile('output.json', JSON.stringify(arr, null, 4), 'utf-8',
+          // function(err){
+          //   if(err){
+          //     console.log("fille error when writing")
+          //   }
+          //  callback2("complete")
+          // })
+        })
+        console.log("orig leng", original_json.length)
     }
   });
 }
--- client/static/js/resultController.js
+++ client/static/js/resultController.js
@@ -4,7 +4,7 @@ app.controller('resultController', function ($scope, $window, ngProgressFactory,
     $window.result[gmv].Sum_of_GMV = parseInt($window.result[gmv].Sum_of_GMV)
   }
 
-  $scope.results = $window.result;
+  // $scope.results = $window.result;
 
   $scope.sortType = 'Sum_of_GMV';
   $scope.sortReverse = true;
@@ -13,40 +13,70 @@ app.controller('resultController', function ($scope, $window, ngProgressFactory,
   $scope.progressbar = ngProgressFactory.createInstance();
   $scope.progressbar.setHeight('13px');
 
-  $scope.myFile = {};
+  $scope.upload_info = {};
   $scope.uploadFile = function(){
     $scope.progressbar.start();
-    var file = $scope.myFile;
     var uploadUrl = "/upload";
-    fileUpload.uploadFileToUrl(file, uploadUrl, function(data){
+
+    fileUpload.uploadFileToUrl($scope.upload_info, uploadUrl, function(data){
       if(data.success == true){
         $scope.progressbar.complete();
-         $scope.progressbar.setColor('green');
+        $scope.progressbar.setColor('green');
+        $scope.filename = data.file_name;
       }
     });
   };
 
-  $scope.get_results = function(){
-
+  $scope.get_results = function(data){
+    $scope.progressbar.start();
+    console.log("filename", data);
+    fileUpload.get_results(data, function(res){
+      console.log(res);
+      $scope.progressbar.complete();
+      $scope.results = res.result;
+    })
+  }
+  $scope.search_results = function(data){
+    console.log(data.filename)
+    fileUpload.search_results(data.filename, function(res){
+      console.log(res.result.result)
+      $scope.results = res.result.result;
+    })
   }
 
 });
 
 app.factory('fileUpload', function($http){
   var factory = {};
+
   factory.uploadFileToUrl = function(file, uploadUrl, callback){
     var fd = new FormData();
-    fd.append('file', file);
+    for(var key in file){
+      fd.append(key, file[key]);
+    }
     console.log("FILE", file)
     $http.post(uploadUrl, fd, {
-        transformRequest: angular.identity,
-        headers: {'Content-Type': undefined}
+      transformRequest: angular.identity,
+      headers: {'Content-Type': undefined}
     })
     .success(function(output){
       callback(output);
     })
     .error(function(){
     });
+  }
+
+  factory.get_results = function(filename, callback){
+    $http.get('/get_results/'+filename).success(function(output){
+      callback(output);
+    })
+  }
+
+  factory.search_results = function(filename, callback){
+    console.log("insidefac")
+    $http.get('/search_results/' + filename).success(function(output){
+      callback(output);
+    })
   }
   return factory;
 })
